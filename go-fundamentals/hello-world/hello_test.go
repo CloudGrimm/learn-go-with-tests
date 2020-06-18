f86dee7f8f778324a63e73bd9dce0c478b5d4ab4
Tinashe Wilbrod Chipomho <wchipomho@gmail.com>
2020-06-18 14:57:42 +0200

    refactored hello word with tests

--- go-fundamentals/hello-world/hello_test.go
+++ go-fundamentals/hello-world/hello_test.go
@@ -10,15 +10,29 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
+
+	//test in english
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Wilbrod")
+		got := Hello("Wilbrod", "English")
 		want := "Hello, Wilbrod"
 		assertCorrectMessage(t, got, want)
 	})
-
+//test for an empty string
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	//test for spanish
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Wilbrod", "Spanish")
+		want := "Hola, Wilbrod"
+		assertCorrectMessage(t, got, want)
+	})
+	//test for French
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Wilbrod", "French")
+		want := "Bonjour, Wilbrod"
 		assertCorrectMessage(t, got, want)
 	})
 
