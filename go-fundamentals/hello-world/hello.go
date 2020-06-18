7f51ef4d14ce87a01dd6781997755f482a118081
Tinashe Wilbrod Chipomho <wchipomho@gmail.com>
2020-06-18 13:39:57 +0200

    add refactored assertion into function

--- go-fundamentals/hello-world/hello.go
+++ go-fundamentals/hello-world/hello.go
@@ -2,8 +2,15 @@ package main // packages are a way of grouping up related Go code together
 
 import "fmt"
 
+const englishHelloPrefix  = "Hello, "
+
 func Hello(name string) string{
-	return "Hello, " + name
+	if name != ""{
+		return englishHelloPrefix + name
+	}else{
+		return englishHelloPrefix + "World"
+	}
+
 }
 func main(){
 	fmt.Println(Hello("Wilbrod"))
