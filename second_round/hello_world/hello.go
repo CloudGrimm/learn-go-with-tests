8e2b2ce57c03b267d43d46484838d2fa4ae8dfab
Tinashe Wilbrod Chipomho <wchipomho@gmail.com>
2020-09-29 11:55:39 +0200

    refactored tests and domain code

--- second_round/hello_world/hello.go
+++ second_round/hello_world/hello.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+//writing constants
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
