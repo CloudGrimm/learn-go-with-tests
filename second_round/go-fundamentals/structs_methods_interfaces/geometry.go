4954e9020011b1ab40299a0de0a67da3ca8fd022
Tinashe Wilbrod Chipomho <wchipomho@gmail.com>
2020-10-02 18:35:12 +0200

    intial setup on structs, methods and interfaces

---
+++ second_round/go-fundamentals/structs_methods_interfaces/geometry.go
@@ -0,0 +1,11 @@
+package main
+
+//Perimeter calculates area of rectangle
+func Perimeter(length, height float64) float64 {
+	return 2 * (length + height)
+}
+
+//Area calculates area of rectangle
+func Area(length, height float64) float64 {
+	return length * height
+}
