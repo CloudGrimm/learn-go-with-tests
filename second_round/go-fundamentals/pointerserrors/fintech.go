3bb5e50d0da938ac2607f3e12a856d62b73abd1b
Tinashe Wilbrod Chipomho <wchipomho@gmail.com>
2020-10-04 15:16:44 +0200

    update

--- second_round/go-fundamentals/pointerserrors/fintech.go
+++ second_round/go-fundamentals/pointerserrors/fintech.go
@@ -1,16 +1,31 @@
 package pointerserrors
 
+import "fmt"
+
+// Bitcoin of type int
+type Bitcoin int
+
 // Wallet struct to define data to be stored
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit into the wallet
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Balance for the wallet
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+// String method to make use of stringer interface
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+// Withdraw from the wallet
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
