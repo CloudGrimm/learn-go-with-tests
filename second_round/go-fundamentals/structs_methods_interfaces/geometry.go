cc39a9f1666ee154b89beb7df51863310632ceb3
Tinashe Wilbrod Chipomho <wchipomho@gmail.com>
2020-10-04 06:38:21 +0200

    add table based tests

--- second_round/go-fundamentals/structs_methods_interfaces/geometry.go
+++ second_round/go-fundamentals/structs_methods_interfaces/geometry.go
@@ -1,11 +1,45 @@
 package main
 
+import "math"
+
+//Rectangle struct to define its data
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+//Circle struct to define its data
+type Circle struct {
+	Radius float64
+}
+
+// Triangle struct to define its data
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+// Shape inteface
+type Shape interface {
+	Area() float64
+}
+
 //Perimeter calculates area of rectangle
-func Perimeter(length, height float64) float64 {
-	return 2 * (length + height)
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
 //Area calculates area of rectangle
-func Area(length, height float64) float64 {
-	return length * height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Area method for circle
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Area method for Triangle
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
