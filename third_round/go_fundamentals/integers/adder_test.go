a6eb888aa74e283706328cfa30539603dbd1f164
Tinashe Wilbrod Chipomho <wchipomho@gmail.com>
2020-12-02 12:29:12 +0200

    work with integers

---
+++ third_round/go_fundamentals/integers/adder_test.go
@@ -0,0 +1,15 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	got := Add(1, 2)
+	want := 3
+
+	if got != want {
+		fmt.Errorf("got %d but want %d", got, want)
+	}
+}
