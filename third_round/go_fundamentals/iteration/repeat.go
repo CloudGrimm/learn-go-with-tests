df93977b4b48eedac96d88d25bfeb593093d0131
Tinashe Wilbrod Chipomho <wchipomho@gmail.com>
2020-12-02 15:21:43 +0200

    add benchmarks

--- third_round/go_fundamentals/iteration/repeat.go
+++ third_round/go_fundamentals/iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
 //Repeat function to repeat a character many times requested
-func Repeat(char string) (repeated string) {
-	for i := 0; i < 5; i++ {
+func Repeat(char string, times int) (repeated string) {
+	for i := 0; i < times; i++ {
 		repeated += char
 	}
 	return
