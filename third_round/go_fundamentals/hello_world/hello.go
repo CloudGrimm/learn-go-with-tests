cb13af104991c04f8a50cc6f3acfba9cd73d2281
Tinashe Wilbrod Chipomho <wchipomho@gmail.com>
2020-12-02 08:42:12 +0200

    refactor french consts

--- third_round/go_fundamentals/hello_world/hello.go
+++ third_round/go_fundamentals/hello_world/hello.go
@@ -5,14 +5,16 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const spanish = "Spanish"
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
 
 //Hello function that returns english greeting
 func Hello(name, lang string) string {
 	if lang == spanish {
 		return spanishHelloPrefix + name
 	}
-	if lang == "French" {
-		return "Bonjour, " + name
+	if lang == french {
+		return frenchHelloPrefix + name
 	}
 	if name == "" {
 		name = "World"
