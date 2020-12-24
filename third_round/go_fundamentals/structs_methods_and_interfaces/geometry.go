7893fa339cf5cb001e33f3e6e169b5ace7ff13b3
Tinashe Chipomho <tchipomh@redhat.com>
2020-12-24 15:55:50 +0200

    introduction to structs

---
+++ third_round/go_fundamentals/structs_methods_and_interfaces/geometry.go
@@ -0,0 +1,18 @@
+package main
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+//Perimeter function to calculate the perimeter of provided values
+func Perimeter(rectangle Rectangle) float64 {
+	res := 2 * (rectangle.Width + rectangle.Height)
+	return res
+}
+
+//Area of a rectangle calculation
+func Area(rectangle Rectangle) float64 {
+	res := rectangle.Width * rectangle.Height
+	return res
+}
