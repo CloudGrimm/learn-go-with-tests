0913b2ae40bf0df985c549c413e0d1a27f4db183
Tinashe Chipomho <tchipomh@redhat.com>
2020-12-24 12:12:35 +0200

    repo update

---
+++ tour_of_go/anatomy_of_arrays.go
@@ -0,0 +1,4 @@
+package main
+
+// An aray is a collection of the same data type
+// It is of fixed length
--- tour_of_go/anatomy_of_functions.go
+++ tour_of_go/anatomy_of_functions.go
@@ -22,11 +22,23 @@ func add(a, b int) int64 {
 	return int64(c)
 }
 
-//Multiple return values
-func addMulti(a, b int) (int, int) {
-	return a + b, a * b
+//Multiple return values with named return values
+func addMulti(a, b int) (add, multi int) {
+	add = a + b
+	multi = a * b
+	return
 }
 
+// Recursive function is that function that calls itself from inside the body.
+// n! = n*(n-1)! where n > 0
+
+func getFactorial(num int) int {
+	if num > 1 {
+		return num * getFactorial(num-1)
+	} else {
+		return 1
+	}
+}
 func main() {
 	dosomething()
 	greet("Tinashe")
@@ -37,4 +49,5 @@ func main() {
 	//using the blank identifier in Go
 	_, multiRes2 := addMulti(1, 5)
 	fmt.Println(multiRes2)
+	fmt.Println(getFactorial(2))
 }
