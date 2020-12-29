5197b96a8709abe28d11f064b4e9501d5bd00556
Tinashe Chipomho <tchipomh@redhat.com>
2020-12-29 09:07:17 +0200

    update

--- third_round/go_fundamentals/structs_methods_and_interfaces/geometry.go
+++ third_round/go_fundamentals/structs_methods_and_interfaces/geometry.go
@@ -5,6 +5,10 @@ type Rectangle struct {
 	Height float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
 //Perimeter function to calculate the perimeter of provided values
 func Perimeter(rectangle Rectangle) float64 {
 	res := 2 * (rectangle.Width + rectangle.Height)
