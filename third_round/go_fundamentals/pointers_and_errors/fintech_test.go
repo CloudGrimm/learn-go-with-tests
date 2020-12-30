68350f7ea19778979b7e950be00cb778890319f4
Tinashe Chipomho <tchipomh@redhat.com>
2020-12-30 12:35:10 +0200

    refactor duplication in tests

--- third_round/go_fundamentals/pointers_and_errors/fintech_test.go
+++ third_round/go_fundamentals/pointers_and_errors/fintech_test.go
@@ -4,28 +4,28 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
+	assertCorrectMessage := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s ", got, want)
+		}
+
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(10)
-
-		got := wallet.Balance()
 		want := Bitcoin(10)
+		assertCorrectMessage(t, wallet, want)
 
-		if got != want {
-			t.Errorf("got %s want %s ", got, want)
-		}
 	})
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
 		wallet.Withdraw(10)
-
-		got := wallet.Balance()
 		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertCorrectMessage(t, wallet, want)
 	})
 }
