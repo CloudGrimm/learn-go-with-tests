801eb601a5d96279a45d180cf9223f15a3dac5db
Wilbrod <wchipomho@gmail.com>
2021-04-11 16:37:31 +0200

    adding shona lang

--- fourth_round/hello_world/hello.go
+++ fourth_round/hello_world/hello.go
@@ -3,14 +3,24 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const spanish = "Spanish"
+const shona = "Shona"
+const shonaHelloPrefix = "Zvirisei, "
 
-func Hello(name string) string {
+func Hello(name, lang string) string {
 	if name == ""{
 		name = "World"
+	}
+	if lang == spanish {
+		return spanishHelloPrefix + name
+	}
+	if lang == shona {
+		return shonaHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
