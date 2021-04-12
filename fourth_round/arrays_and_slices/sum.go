3252331a642dd3b01235be7c84e3f965a62c4a75
Wilbrod <wchipomho@gmail.com>
2021-04-12 11:45:20 +0200

    refactor slice using append

--- fourth_round/arrays_and_slices/sum.go
+++ fourth_round/arrays_and_slices/sum.go
@@ -4,5 +4,13 @@ func Sum(x []int) (sum int){
 	for _, number := range x{
 		sum += number
 	}
-	return sum
+	return
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int){
+
+	for _, numbers := range numbersToSum{
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
 }
