84b9f9f9847dae21f89d836d37c9b2064ec365ef
Wilbrod <wchipomho@gmail.com>
2021-04-12 11:55:05 +0200

    add SumAllTails

--- fourth_round/arrays_and_slices/sum.go
+++ fourth_round/arrays_and_slices/sum.go
@@ -12,5 +12,12 @@ func SumAll(numbersToSum ...[]int) (sums []int){
 	for _, numbers := range numbersToSum{
 		sums = append(sums, Sum(numbers))
 	}
-	return sums
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int){
+	for _, numbers := range numbersToSum{
+		sums = append(sums, Sum(numbers[1:]))
+	}
+	return
 }
