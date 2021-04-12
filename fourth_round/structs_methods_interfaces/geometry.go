2d1d3b27052efd59d9cedeecb7dce51cc66a07bd
Wilbrod <wchipomho@gmail.com>
2021-04-12 18:06:27 +0200

    add structs, methods

---
+++ fourth_round/structs_methods_interfaces/geometry.go
@@ -0,0 +1,25 @@
+package main
+
+import "math"
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+type Circle struct{
+	Radius float64
+}
+func Perimeter(rec Rectangle) (per float64){
+	return 2 * (rec.Width + rec.Height)
+}
+
+func (rec Rectangle) Area() (area float64){
+	area = rec.Width * rec.Height
+	return
+}
+
+func (cir Circle) Area() (area float64){
+	area = math.Pi * cir.Radius * cir.Radius
+	return
+}
---
+++ fourth_round/structs_methods_interfaces/go.mod
@@ -0,0 +1,3 @@
+module github.com/cloudgrimm/learn-go-with-tests/fourth_round/structs_methods_interfaces
+
+go 1.16
