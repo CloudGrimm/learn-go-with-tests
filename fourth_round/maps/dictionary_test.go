e89fed0dc1acaf5309cf75f5b2e2ab5dce277231
Wilbrod <wchipomho@gmail.com>
2021-05-24 13:32:56 +0200

    add dictionary.go and intial refactor

--- fourth_round/maps/dictionary_test.go
+++ fourth_round/maps/dictionary_test.go
@@ -8,7 +8,12 @@ func TestSearch(t *testing.T){
 	got := Search(dictionary, "test")
 	want := "this is just a test"
 
-	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+
+	assertMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q given, %q", got, want, "test")
+		}
 	}
+	assertMessage(t, got, want)
 }
---
+++ fourth_round/maps/go.mod
@@ -0,0 +1,3 @@
+module github.com/cloudgrimm/learn-go-with-tests/fourth_round/maps
+
+go 1.16
